4757f5978992c7a8c8f187d26493fa529464329d
Casuke <kylellamas723@gmail.com>
2026-03-30 12:20:04 +0800

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -6,39 +6,23 @@
 
 return {
     ["Minify"] = {
-        -- The default LuaVersion is Lua51
         LuaVersion = "Lua51";
-        -- For minifying no VarNamePrefix is applied
         VarNamePrefix = "";
-        -- Name Generator for Variables
         NameGenerator = "MangledShuffled";
-        -- No pretty printing
         PrettyPrint = false;
-        -- Seed is generated based on current time
         Seed = 0;
-        -- No obfuscation steps
-        Steps = {
-
-        }
+        Steps = {}
     };
     ["Vm"] = {
-        -- The default LuaVersion is Lua51
         LuaVersion = "Lua51";
-        -- For minifying no VarNamePrefix is applied
         VarNamePrefix = "";
-        -- Name Generator for Variables that look like this: IlI1lI1l
         NameGenerator = "MangledShuffled";
-        -- No pretty printing
         PrettyPrint = false;
-        -- Seed is generated based on current time
         Seed = 0;
-        -- Obfuscation steps
         Steps = {
             {
                 Name = "Vmify";
-                Settings = {
- 
-                };
+                Settings = {};
             },
             {
                 Name = "WrapInFunction";
@@ -48,132 +32,4 @@ return {
             }
         }
     };
-    ["Weak"] = {
-        -- The default LuaVersion is Lua51
-        LuaVersion = "Lua51";
-        -- For minifying no VarNamePrefix is applied
-        VarNamePrefix = "";
-        -- Name Generator for Variables that look like this: IlI1lI1l
-        NameGenerator = "MangledShuffled";
-        -- No pretty printing
-        PrettyPrint = false;
-        -- Seed is generated based on current time
-        Seed = 0;
-        -- Obfuscation steps
-        Steps = {
-            {
-                Name = "ConstantArray";
-                Settings = {
-                    Treshold    = 1;
-                    StringsOnly = true;
-                }
-            },
-        }
-    };
-    ["Medium"] = {
-        -- The default LuaVersion is Lua51
-        LuaVersion = "Lua51";
-        -- For minifying no VarNamePrefix is applied
-        VarNamePrefix = "";
-        -- Name Generator for Variables that look like this: IlI1lI1l
-        NameGenerator = "MangledShuffled";
-        -- No pretty printing
-        PrettyPrint = false;
-        -- Seed is generated based on current time
-        Seed = 0;
-        -- Obfuscation steps
-        Steps = {
-            {
-                Name = "SplitStrings";
-                Settings = {
-                    Treshold = 1;
-                    MinLength = 15;
-                    MaxLength = 40;
-                    ConcatenationType = "custom";
-                    CustomFunctionType = "local";
-                    CustomLocalFunctionsCount = 2;
-                }
-            },
-            {
-                Name = "ConstantArray";
-                Settings = {
-                    Treshold    = 1;
-                    StringsOnly = true;
-                    Shuffle     = true;
-                    Rotate      = true;
-                    LocalWrapperTreshold = 1;
-                    LocalWrapperCount = 3;
-                    LocalWrapperArgCount = 3;
-                }
-            },
-            {
-                Name = "ProxifyLocals";
-                Settings = {
-                    
-                }
-            },
-            {
-                Name = "WrapInFunction";
-                Settings = {
-                    Iterations = 1;
-                }
-            }
-        }
-    };
-    ["Strong"] = {
-        -- The default LuaVersion is Lua51
-        LuaVersion = "Lua51";
-        -- For minifying no VarNamePrefix is applied
-        VarNamePrefix = "";
-        -- Name Generator for Variables that look like this: IlI1lI1l
-        NameGenerator = "MangledShuffled";
-        -- No pretty printing
-        PrettyPrint = false;
-        -- Seed is generated based on current time
-        Seed = 0;
-        -- Obfuscation steps
-        Steps = {
-            {
-                Name = "Vmify";
-                Settings = {
-
-                };
-            },
-            {
-                Name = "SplitStrings";
-                Settings = {
-                    Treshold = 1;
-                    MinLength = 15;
-                    MaxLength = 40;
-                    ConcatenationType = "custom";
-                    CustomFunctionType = "local";
-                    CustomLocalFunctionsCount = 2;
-                }
-            },
-            {
-                Name = "ConstantArray";
-                Settings = {
-                    Treshold    = 1;
-                    StringsOnly = true;
-                    Shuffle     = true;
-                    Rotate      = true;
-                    LocalWrapperTreshold = 1;
-                    LocalWrapperCount = 3;
-                    LocalWrapperArgCount = 3;
-                }
-            },
-            {
-                Name = "ProxifyLocals";
-                Settings = {
-                    
-                }
-            },
-            {
-                Name = "WrapInFunction";
-                Settings = {
-                    Iterations = 1;
-                }
-            }
-        }
-    }
 }
