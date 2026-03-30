c22a76d5c0ae8c249f4d569c28df9c35f0e99694
Casuke <kylellamas723@gmail.com>
2026-03-30 13:25:18 +0800

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -1,9 +1,4 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- pipeline.lua
---
--- This Script Provides some configuration presets
-
+-- I only Copy the Two of Presets of Original Prometheus because it's too hard to make.
 return {
     ["Minify"] = {
         LuaVersion = "Lua51";
